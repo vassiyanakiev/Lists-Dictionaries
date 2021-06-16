50d24421f5c60e6b866b480151322072c4270a73
Vasilena Georgieva <75674763+vassiyanakiev@users.noreply.github.com>
2021-06-16 15:42:02 +0300

    lists and dictionaries

---
+++ MergingLists/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MergingLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> secondList = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> numbers = new List<int>(firstList.Count + secondList.Count);
+            int limit = Math.Min(firstList.Count, secondList.Count);
+
+            for (int i = 0; i < limit; i++)
+            {
+                numbers.Add(firstList[i]);
+                numbers.Add(secondList[i]);
+            }
+
+            if (firstList.Count != secondList.Count)
+            {
+                if (firstList.Count > secondList.Count)
+                {
+                    numbers.AddRange(GetLastList(firstList, secondList));
+                }
+                else
+                {
+                    numbers.AddRange(GetLastList(secondList, firstList));
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers)); 
+        }
+
+        private static List<int> GetLastList(List<int> longerList, List<int> shorterList)
+        {
+            if (longerList.Count <= shorterList.Count)
+            {
+                throw new ArgumentException();
+            }
+
+            List<int> result = new List<int>();
+
+            for (int i = shorterList.Count; i < longerList.Count; i++)
+            {
+                result.Add(longerList[i]);
+            }
+
+            return result;
+        }
+    }
+}
